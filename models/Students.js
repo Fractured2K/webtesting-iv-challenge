cb7facab313465b503fd05f7006409c86bff9094
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-11 13:51:20 -0700

    Create find student method

--- models/Students.js
+++ models/Students.js
@@ -2,12 +2,19 @@ const db = require('../database/dbConfig');
 
 module.exports = {
 	create,
+	find,
 	remove
 };
 
 async function create(student) {
 	const [id] = await db('students').insert(student);
 
+	return db('students')
+		.where({ id })
+		.first();
+}
+
+async function find(id) {
 	return db('students')
 		.where({ id })
 		.first();
