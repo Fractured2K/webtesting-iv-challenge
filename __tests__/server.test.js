6d68ee257d158d9b962426d551c11d37c0a93015
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-11 13:42:13 -0700

    Handle empty student

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const server = require('./server');
+const server = require('../server');
 const db = require('../database/dbConfig');
 
 describe('Server.js tests', () => {
@@ -21,7 +21,7 @@ describe('Server.js tests', () => {
 		it('should create a student and respond with 201', async () => {
 			const student = { name: 'John Smith' };
 			const response = await request(server)
-				.post('/student')
+				.post('/students')
 				.send(student);
 
 			expect(response.status).toBe(201);
@@ -30,7 +30,7 @@ describe('Server.js tests', () => {
 		it('should respond with 400 when body formatting is incorrect', async () => {
 			const student = {};
 			const response = await request(server)
-				.post('/student')
+				.post('/students')
 				.send(student);
 
 			expect(response.status).toBe(400);
@@ -39,6 +39,10 @@ describe('Server.js tests', () => {
 
 	describe('DELETE /students/:id', () => {
 		it('should delete student and respond with 200', async () => {
+			await request(server)
+				.post('/students')
+				.send({ name: 'name' });
+
 			const response = await request(server).delete(
 				'/students/1'
 			);
--- server.js
+++ server.js
@@ -12,6 +12,11 @@ server.get('/', (req, res) =>
 
 server.post('/students', async (req, res) => {
 	try {
+		if (!req.body.name)
+			return res
+				.status(400)
+				.json({ message: 'Please enter a name' });
+
 		const student = await Students.create(req.body);
 		res.status(201).json(student);
 	} catch (error) {
@@ -31,3 +36,5 @@ server.delete('/students/:id', async (req, res) => {
 server.listen(5000, () =>
 	console.log('Server running on http://localhost:5000')
 );
+
+module.exports = server;
