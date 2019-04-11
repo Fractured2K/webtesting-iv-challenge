de8f25c8e84da5c1fbcbbddec54c8238bb2def8b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-11 12:53:49 -0700

    Create student post endpoint tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,4 +9,26 @@ describe('Server.js tests', () => {
 				.expect(200);
 		});
 	});
+
+	describe('POST /students', () => {
+		it('should respond create student and respond with 201', async () => {
+			const student = { name: 'John Smith' };
+
+			const response = await request(server)
+				.post('/student')
+				.send(student);
+
+			expect(response.status).toBe(201);
+		});
+
+		it('should respond with 400 when missing body data', async () => {
+			const student = {};
+
+			const response = await request(server)
+				.post('/student')
+				.send(student);
+
+			expect(response.status).toBe(400);
+		});
+	});
 });
