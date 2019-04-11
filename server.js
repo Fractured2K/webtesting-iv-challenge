5a36e7129e30dba768599c0736cec92c335646ab
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-11 13:16:11 -0700

    Create sanity check endpoint

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) =>
+	res.status(200).json({ message: 'Sanity check' })
+);
+
 server.listen(5000, () =>
 	console.log('Server running on http://localhost:5000')
 );
